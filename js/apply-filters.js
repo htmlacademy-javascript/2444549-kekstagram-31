216044667b6ac2a350ffcdb5bc38720d15a8c8b6
AlexZheltov93 <alexseeya93@yandex.ru>
2024-04-04 17:08:21 +0700

    Кексаграмм на проверку

---
+++ js/apply-filters.js
@@ -0,0 +1,127 @@
+const effectsList = document.querySelector('.effects__list');
+const effectsItems = effectsList.querySelectorAll('.effects__item');
+const picturePreview = document.querySelector('.img-upload__preview img');
+const effectValue = document.querySelector('.effect-level__value');
+const effectSlider = document.querySelector('.effect-level__slider');
+const effectLevel = document.querySelector('.img-upload__effect-level');
+
+const addCheckHandler = (effectItem) => {
+  const effectsRadio = effectItem.querySelector('.effects__radio');
+
+  effectsRadio.addEventListener('click', () => {
+    picturePreview.className = '';
+    picturePreview.classList.add(`effects__preview--${effectsRadio.value}`);
+    effectSlider.setAttribute('disabled', true);
+  });
+};
+
+noUiSlider.create(effectSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+
+const changeLevelEffect = () => {
+
+  switch(picturePreview.className) {
+    case 'effects__preview--chrome':
+      picturePreview.style.filter = `grayscale(${effectValue.value})`;
+      break;
+    case 'effects__preview--sepia':
+      picturePreview.style.filter = `sepia(${effectValue.value})`;
+      break;
+    case 'effects__preview--marvin':
+      picturePreview.style.filter = `invert(${effectValue.value}%)`;
+      break;
+    case 'effects__preview--heat':
+      picturePreview.style.filter = `brightness(${effectValue.value})`;
+      break;
+    case 'effects__preview--phobos':
+      picturePreview.style.filter = `blur(${effectValue.value}px)`;
+      break;
+    case 'effects__preview--none':
+      picturePreview.style.filter = 'none';
+      break;
+  }
+};
+
+effectSlider.noUiSlider.on('update', () => {
+  effectValue.value = effectSlider.noUiSlider.get();
+  changeLevelEffect();
+});
+
+
+const changeSliderRange = (effectItem) => {
+  const effectsRadio = effectItem.querySelector('.effects__radio');
+  effectSlider.setAttribute('disabled', true);
+  effectLevel.style.display = 'none';
+
+  effectsRadio.addEventListener('click', () =>{
+    if (picturePreview.className === 'effects__preview--chrome' || picturePreview.className === 'effects__preview--sepia') {
+      effectSlider.removeAttribute('disabled');
+      effectLevel.style.display = 'block';
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
+    } else if (picturePreview.className === 'effects__preview--marvin') {
+      effectSlider.removeAttribute('disabled');
+      effectLevel.style.display = 'block';
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1,
+      });
+    } else if (picturePreview.className === 'effects__preview--heat') {
+      effectSlider.removeAttribute('disabled');
+      effectLevel.style.display = 'block';
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 1,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1,
+      });
+    } else if (picturePreview.className === 'effects__preview--phobos') {
+      effectSlider.removeAttribute('disabled');
+      effectLevel.style.display = 'block';
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1,
+      });
+    } else if (picturePreview.className === 'effects__preview--none') {
+      effectSlider.setAttribute('disabled', true);
+      effectLevel.style.display = 'none';
+      effectSlider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1,
+        connect: 'lower',
+      });
+    }
+  });
+};
+
+for (let i = 0; i < effectsItems.length; i++) {
+  addCheckHandler(effectsItems[i]);
+  changeSliderRange(effectsItems[i]);
+}
--- js/util.js
+++ js/util.js
@@ -1,6 +1,65 @@
-import { createRandomIdFromRangeGenerator } from './data.js';
+const NUMBER_SYSTEM_CALCULUS = 10;
+const MIN = 0;
 
-//Получаем случайный элемент массива//
-const getRandomArrayElement = (elements) => elements[createRandomIdFromRangeGenerator(0, elements.length - 1)];
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-export { getRandomArrayElement };
+  return Math.floor(result);
+};
+
+const getRandom = (min, max) => {
+  const array = [];
+  return function () {
+    let number = getRandomInteger(min, max);
+    if (array.length >= max - min + 1) {
+      return null;
+    }
+    while (array.includes(number)) {
+      number = getRandomInteger(min, max);
+    }
+    array.push(number);
+    return number;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const openPopup = (popup, onDocumentKeydown) => {
+  popup.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const closePopup = (popup, onDocumentKeydown) => {
+  popup.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const getNumber = (string) => {
+  let number = '';
+  string = string.toString().replaceAll(' ', '');
+  for (let i = 0; i < string.length; i++) {
+    if (isNaN(string[i])) {
+      number += '';
+    } else {
+      number += parseInt(string[i], NUMBER_SYSTEM_CALCULUS);
+    }
+  }
+  if (string < MIN) {
+    number *= 1;
+  }
+  return (number === '') ? 'NaN' : number;
+};
+
+export { getRandomInteger, getRandom, isEscapeKey, openPopup, closePopup, getRandomArrayElement, debounce, getNumber };
